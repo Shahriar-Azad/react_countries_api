68fa7775027300e51caea8f7e53c56681ad07ca3
Shahriar-Azad <shahriar.azad123@gmail.com>
2025-09-18 23:53:02 +0600

    third commit

--- react_countries_api/src/components/Countries/Countries.jsx
+++ react_countries_api/src/components/Countries/Countries.jsx
@@ -1,9 +1,14 @@
+import { use } from "react"
+
 
 const Countries = ({countriesPromise}) => {
+    const countriesData = use(countriesPromise);
+    const countries = countriesData.countries;
+    console.log(countries)
 
   return (
     <div>
-        <h1>In the Countries: </h1>
+        <h1>In the Countries: {countries.length}</h1>
     </div>
   )
 }
